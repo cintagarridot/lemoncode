43fbd41af4eb8a31c3af634e69f0bfbb410f1a07
Cinta <cinta_gt96@hotmail.com>
2023-11-22 22:00:17 +0100

    languages-entregables: exercise 5 finished

--- languages/04-playgrounds/03-playground-ejercicios/src/02-entregables/ejercicio-05.ts
+++ languages/04-playgrounds/03-playground-ejercicios/src/02-entregables/ejercicio-05.ts
@@ -1,1 +1,42 @@
 console.log("************** DELIVERABLE 05 *********************");
+
+class SlothMachine {
+
+    coins;
+
+    constructor() {
+        this.coins = 0;
+    }
+    
+    play() {
+        this.coins += 1;
+        const roulette1 = Math.random() < 0.5;
+        const roulette2 = Math.random() < 0.5;
+        const roulette3 = Math.random() < 0.5;
+        if (roulette1 && roulette2 && roulette3) {
+            const result = `Congratulations!!!. You won ${this.coins} coins!!`;
+            this.coins = 0;
+            return result;
+        }
+
+        return "Good luck next time!!";
+    }
+}
+  
+  const machine1 = new SlothMachine();
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+  console.log('machine1.play()', machine1.play());
+
+
